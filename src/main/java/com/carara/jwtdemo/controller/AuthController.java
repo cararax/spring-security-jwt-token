cc292c4eeebdd55a92ebb667e54817dc01e95af4
Pedro Carara <cararax@gmail.com>
2022-02-07 21:05:35 -0300

    Adicionado endpoint de autenticação(gerar token) e liberada rota de autenticaçao

--- src/main/java/com/carara/jwtdemo/controller/AuthController.java
+++ src/main/java/com/carara/jwtdemo/controller/AuthController.java
@@ -1,13 +1,47 @@
 package com.carara.jwtdemo.controller;
 
-import lombok.Getter;
+import com.carara.jwtdemo.model.JwtRequest;
+import com.carara.jwtdemo.model.JwtResponse;
+import com.carara.jwtdemo.service.UserService;
+import com.carara.jwtdemo.util.JWTUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+    @Autowired
+    private JWTUtility jwtUtility;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "Welcome to the JWT demo.";
+    }
+
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(), jwtRequest.getPassword()));
+        } catch (BadCredentialsException exception) {
+            throw new Exception("INVALID CREDENTIALS", exception);
+        }
+
+        final UserDetails userDetails = userService.loadUserByUsername(jwtRequest.getUsername());
+        final String token = jwtUtility.generateToken(userDetails);
+
+        return new JwtResponse(token);
+
+
     }
 }
---
+++ src/main/java/com/carara/jwtdemo/util/JWTUtility.java
@@ -0,0 +1,78 @@
+package com.carara.jwtdemo.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JWTUtility implements Serializable {
+
+    public static final long serialVersionUID = 1L;
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    //for retrieving any information from token we will need the secret key
+    private String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+    //retrieve expiration date from jwt token
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    private Claims getAllClaimsFromToken(String token){
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    //for retrieving any information from token we will need the secret key
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    //check if the token has expired
+    private Boolean isTokenExpired(String token){
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    //generate token for user
+    public String generateToken(UserDetails userDetails){
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, userDetails.getUsername());
+    }
+
+    //while creating the token
+    //1. Define claims of the thoken, like Issue, expiration, subject and the ID
+    //2. Sign the JWT usin the HS512 algorithm and secret key.
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    //validate token
+    public Boolean validateToken(String token, UserDetails userDetails){
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+}
